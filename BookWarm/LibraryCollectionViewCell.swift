54b3d4fa8f2e833a7724d56630218a93d4b5964f
Kim junhwan <tazanboy1@naver.com>
2023-07-31 20:41:21 +0900

    [feat] 메인화면 콜렉션 뷰 UI 완성

---
+++ BookWarm/LibraryCollectionViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  LibraryCollectionViewCell.swift
+//  BookWarm
+//
+//  Created by JunHwan Kim on 2023/07/31.
+//
+
+import UIKit
+
+class LibraryCollectionViewCell: UICollectionViewCell {
+    
+    enum Metric {
+        static let cornerRadius: CGFloat = 20.0
+    }
+    
+    static let identifier = "LibraryCollectionViewCell"
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var openDateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = Metric.cornerRadius
+    }
+    
+    func configureCell(movie: Movie) {
+        posterImageView.image = UIImage(named: "\(movie.title)")
+        nameLabel.text = movie.title
+        openDateLabel.text = movie.releaseDate
+        nameLabel.adjustsFontSizeToFitWidth = true
+        openDateLabel.adjustsFontSizeToFitWidth = true
+    }
+
+}
---
+++ BookWarm/LibraryCollectionViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LibraryCollectionViewController.swift
+//  BookWarm
+//
+//  Created by JunHwan Kim on 2023/07/31.
+//
+
+import UIKit
+
+class LibraryCollectionViewController: UICollectionViewController {
+    
+    enum Metric {
+        static let numberOfColumns: Int = 2
+        static let inset: CGFloat = 20.0
+    }
+    
+    let MovieList = MovieInfo()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.register(UINib(nibName: LibraryCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: LibraryCollectionViewCell.identifier)
+        layoutCollectionView()
+    }
+    
+    func layoutCollectionView() {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.minimumLineSpacing = Metric.inset
+        flowLayout.minimumInteritemSpacing = Metric.inset
+        flowLayout.sectionInset = UIEdgeInsets(top: Metric.inset, left: Metric.inset, bottom: Metric.inset, right: Metric.inset)
+        let width = UIScreen.main.bounds.width
+        flowLayout.itemSize = CGSize(width: (width-(3*Metric.inset))/2, height: 200)
+        collectionView.collectionViewLayout = flowLayout
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return MovieList.movie.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LibraryCollectionViewCell.identifier, for: indexPath) as? LibraryCollectionViewCell else { return UICollectionViewCell() }
+        cell.configureCell(movie: MovieList.movie[indexPath.row])
+    
+        return cell
+    }
+}
