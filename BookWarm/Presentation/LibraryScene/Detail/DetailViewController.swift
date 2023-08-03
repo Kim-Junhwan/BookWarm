e1ae53af3d4b6fd1a6609307de4d40369e36cc53
Kim junhwan <tazanboy1@naver.com>
2023-08-04 00:08:05 +0900

    [feat] 상세화면 텍스트 뷰 플레이스 홀더 기능 추가

--- BookWarm/Presentation/LibraryScene/Detail/DetailViewController.swift
+++ BookWarm/Presentation/LibraryScene/Detail/DetailViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    enum Sentence {
+        static let memoTextViewPlaceHolder = "메모를 입력해주십시오"
+    }
+    
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var releaseDateLabel: UILabel!
@@ -16,6 +20,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var rateLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
     @IBOutlet weak var dissmissButton: UIButton!
+    @IBOutlet weak var memoTextView: UITextView!
     
     var movie: Movie = Movie(title: "", releaseDate: "", runtime: 0, overview: "", rate: 0.0)
     
@@ -23,6 +28,13 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         configureView()
         checkShowDismissButton()
+        setMemoTextView()
+    }
+    
+    func setMemoTextView() {
+        memoTextView.delegate = self
+        memoTextView.textColor = .gray
+        memoTextView.text = Sentence.memoTextViewPlaceHolder
     }
     
     func checkShowDismissButton() {
@@ -42,5 +54,22 @@ class DetailViewController: UIViewController {
     
     @IBAction func tapDismissButton(_ sender: UIButton) {
         dismiss(animated: true)
+    }
+}
+
+extension DetailViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == Sentence.memoTextViewPlaceHolder && textView.textColor == .gray {
+            textView.text = ""
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textView.text = Sentence.memoTextViewPlaceHolder
+            textView.textColor = .gray
+        }
     }
 }
